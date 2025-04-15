091d8dac03e84e716fe3536c50520377bbefcbc4
Kathy Yost <kathyiyost@gmail.com>
2025-04-14 21:28:56 -0700

    ✅ Added calculator and assignment unit tests, cleaned up unused files

---
+++ AssignmentManagementApp/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace AssignmentManagementApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+
+        public int Subtract(int a, int b) => a - b;
+    }
+}
