261fd1b73d37ce4c50553ec0c54ae1ce8182d78c
Kathy Yost <kathyiyost@gmail.com>
2025-04-18 09:36:53 -0700

    Implement Multiply method and add test cases

---
+++ AssignmentManagementApp/MathHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentManagementApp
+{
+    public class MathHelper
+    {
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
