32a9e712f545e98fe1abc0773674ca2d8aba703e
Clarissa Lopez <lopezclarissa2021@gmail.com>
2025-04-28 10:13:20 -0700

    Add project files.

---
+++ AssignmentManagementApp/Services/AssignmentService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentManagementApp.Services
+{
+    public class AssignmentService
+    {
+        private readonly List<Assignment> assignments = new();
+
+        public void AddAssignment(Assignment assignment)
+        {
+            if (assignment == null)
+            {
+                throw new ArgumentNullException(nameof(assignment));
+            }
+            assignments.Add(assignment);
+        }
+
+        public List<Assignment> GetAllAssignments()
+        {
+            return assignments;
+        }
+
+        public Assignment GetAssignmentById(int id)
+        {
+            return assignments.FirstOrDefault(a => a.Id == id);
+        }
+
+    }
+}
